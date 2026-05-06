9087d25421f57601361715c4e73e8e1d4b8b9000
Jakob Kirkegaard Wibroe <jkwibroe91@gmail.com>
2026-05-06 10:58:48 +0200

    start på opsætning

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import asyncio
+from datetime import datetime
 import logging
 import sys
 
 from automation_server_client import AutomationServer, Workqueue, WorkItemError, Credential, WorkItemStatus
+from odk_tools.tracking import Tracker
+from xflow_client import XFlowClient, ProcessClient
+from kmd_nexus_client.tree_helpers import (
+    filter_by_predicate,
+)
+
+xflow_client: XFlowClient
+xflow_process_client: ProcessClient
 
 
 async def populate_queue(workqueue: Workqueue):
     logger = logging.getLogger(__name__)
 
     logger.info("Hello from populate workqueue!")
+
+    xlow_søge_query = {
+        "text": "",
+        "processTemplateIds": ["753"], # skal have id fra benner
+        "startIndex": 0,
+        "createdDateFrom": "01-01-1980",
+        "createdDateTo": datetime.today().strftime("%d-%m-%Y"),
+    }
+
+    igangværende_processer = xflow_process_client.search_processes_by_current_activity(
+        query=xlow_søge_query,
+        activity_name="RPAIntegration",
+    )
+
+    for proces in igangværende_processer:
+        blanketter = proces["blanketter"]
+
+        samlet_ansøgning = filter_by_predicate(
+            roots=blanketter, predicate=lambda x: x["blanketnavn"] == "SBH - Samlet" # skal have navn fra benner
+        )
 
 
 async def process_workqueue(workqueue: Workqueue):
@@ -34,6 +63,19 @@ if __name__ == "__main__":
     workqueue = ats.workqueue()
 
     # Initialize external systems for automation here..
+    xflow_credential = Credential.get_credential("Xflow - produktion")
+    tracking_credential = Credential.get_credential("Odense SQL Server")
+
+    xflow_client = XFlowClient(
+        token=xflow_credential.password,
+        instance=xflow_credential.data["instance"],
+    )
+    xflow_process_client = ProcessClient(xflow_client)
+
+    tracker = Tracker(
+        username=tracking_credential.username, password=tracking_credential.password
+    )
+
 
     # Queue management
     if "--queue" in sys.argv:
